f134ed212f420144fb058216186b77f9b2ab4560
AdamCSmithCWS <adamcsmith.birdstats@gmail.com>
2022-07-24 12:34:14 -0400

    basic plotting and trend script

---
+++ plotting_w_bbsBayes.R
@@ -0,0 +1,46 @@
+## plotting
+
+library(bbsBayes)
+library(tidyverse)
+library(cmdstanr)
+
+species <- "Golden-winged Warbler"
+
+
+species_f <- gsub(species,pattern = " ",replacement = "_")
+model <- "gamye"
+out_base <- paste(species_f,model,"BBS",sep = "_")
+output_dir <- "output"
+
+load(paste0(output_dir,"/",out_base,"_fit.RData"))
+
+
+inds <- generate_indices(jags_mod = stanfit,
+                         jags_data = stan_data,
+                         backend = "Stan",
+                         stratify_by = "bbs_usgs")
+
+
+trajs <- plot_indices(inds,add_observed_means = TRUE,add_number_routes = TRUE)
+pdf("temp_trajectories.pdf",
+    width = 8,
+    height = 8)
+for(i in 1:length(trajs)){
+  print(trajs[[i]])
+}
+dev.off()
+
+
+trends <- generate_trends(inds)
+trends_short <- generate_trends(inds,Min_year = 2009)
+
+map <- generate_map(trends,select = TRUE,stratify_by = "bbs_usgs",species = species)
+print(map)
+mapshort <- generate_map(trends_short,select = TRUE,stratify_by = "bbs_usgs",species = species)
+print(mapshort)
+
+
+## doesn't yet work
+gfacet <- geofacet_plot(inds,select = TRUE,trends = trends,add_observed_means = TRUE,stratify_by = "bbs_usgs")
+
+
